fd0822104f6c7cd9d5f0db3b6e9e59e96c83f567
TungCD <tungcd@metacity.inc>
2022-07-25 10:03:35 +0700

    authentication

--- app.js
+++ app.js
@@ -25,26 +25,26 @@ app.use("/upload", express.static("upload"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// app.use((req, res, next) => {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header(
-//         "Access-Control-Allow-Headers",
-//         "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-//     );
-//     if (req.method === "OPTIONS") {
-//         res.header("Access-Control-Allow-Methods", 'PUT, POST, PATCH, DELETE, GET');
-//         return res.status(200).json({});
-//     }
-// });
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if (req.method === "OPTIONS") {
+        res.header("Access-Control-Allow-Methods", 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+});
 
 app.use(
   "/api/products",
-  //  middleware,
+   middleware,
   productsRoute
 );
 app.use(
   "/api/orders",
-  //  middleware,
+   middleware,
   ordersRoute
 );
 app.use("/api/users", usersRoute);
